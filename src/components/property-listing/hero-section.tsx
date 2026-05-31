6adb7d04a84c0d03c6fdac69d790215d1531a5e1
adrian-villamil <adrianvillamil94@gmail.com>
2026-05-31 14:03:12 -0500

    update HeroSection component to center content with max-width styling

--- src/components/property-listing/hero-section.tsx
+++ src/components/property-listing/hero-section.tsx
@@ -1,7 +1,7 @@
 export const HeroSection = () => {
   return (
     <section className="h-77.5 w-full bg-[url('/property-listing/hero-image.jpg')] bg-cover bg-center bg-no-repeat px-16 pt-20 md:h-155 md:px-35 md:pt-45">
-      <div className="space-y-2">
+      <div className="mx-auto w-full max-w-284 space-y-2">
         <h1 className="max-w-75 text-4xl leading-[normal] font-semibold md:max-w-md md:text-[64px]">
           Peace, nature, dream
         </h1>
